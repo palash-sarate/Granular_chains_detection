0c72b20bbd07c7591cca4845820edac8df745f2c
Palash <99325824+palash-sarate@users.noreply.github.com>
2024-12-17 02:46:01 +0530

    feat(labeller): add tooltip functionality for visibility and action toggles

--- labelling_tool/labeller.py
+++ labelling_tool/labeller.py
@@ -8,7 +8,33 @@ import cv2
 import threading  # Import threading module
 import cupy as cp  # Import cupy for GPU computations
 # import math
+class Tooltip:
+    def __init__(self, widget, text):
+        self.widget = widget
+        self.text = text
+        self.tooltip_window = None
+        widget.bind("<Enter>", self.show_tooltip)
+        widget.bind("<Leave>", self.hide_tooltip)
 
+    def show_tooltip(self, event):
+        if self.tooltip_window or not self.text:
+            return
+        x, y, _, _ = self.widget.bbox("insert")
+        x += self.widget.winfo_rootx() + 25
+        y += self.widget.winfo_rooty() + 25
+        self.tooltip_window = tw = tk.Toplevel(self.widget)
+        tw.wm_overrideredirect(True)
+        tw.wm_geometry(f"+{x}+{y}")
+        label = tk.Label(tw, text=self.text, justify='left',
+                         background="#ffffe0", relief='solid', borderwidth=1,
+                         font=("tahoma", "8", "normal"))
+        label.pack(ipadx=1)
+
+    def hide_tooltip(self, event):
+        tw = self.tooltip_window
+        self.tooltip_window = None
+        if tw:
+            tw.destroy()
 class ParticleLabelingApp:
     def __init__(self, master):
         self.finding_pairs = False
@@ -101,6 +127,7 @@ class ParticleLabelingApp:
                                        on, off)          
         )
         self.visibility_toggle.pack(anchor='nw', pady=(0, 5))
+        Tooltip(self.visibility_toggle, "Toggle particle visibility")
         
         delete_label = tk.Label(self.left_frame,text="Delete Particle Location")
         delete_label.pack()   
@@ -115,6 +142,7 @@ class ParticleLabelingApp:
                                        on, off),            
         )
         self.delete_location_toggle.pack(anchor='nw', pady=(0, 5))
+        Tooltip(self.delete_location_toggle, "Once on click on a particle to delete")
         
         add_label = tk.Label(self.left_frame,text="Add Particle Location")
         add_label.pack() 
@@ -129,6 +157,7 @@ class ParticleLabelingApp:
                                        on, off),            
         )
         self.add_location_toggle.pack(anchor='nw', pady=(0, 5))
+        Tooltip(self.add_location_toggle, "Once on click on the canvas to add a particle")
     
     def create_Canvas(self):
         # Create a frame for the canvas and scrollbars
